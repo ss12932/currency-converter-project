bc8c6badc3cee73a5ee4c83454cac37b0acf9cf0
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-21 00:18:30 +0100

    fix script tag

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const currencies = {
   CHF: "Swiss Franc",
   CAD: "Canadian Dollar",
@@ -5,29 +7,21 @@ const currencies = {
   GBP: "British Pound",
   JPN: "Japanese Yen",
   USD: "United States Dollar",
-}
-
-
-
-const select = document.querySelectorAll('.currency');
-const number = document.getElementById('text');
-const output = document.getElementById('output-amt');
-
-
-
-
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com',
-		'X-RapidAPI-Key': '077f3a113fmsh70b5629f45bcfa2p114632jsnec97fbc6b6a9'
-	}
 };
 
-fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/USD', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const select = document.querySelectorAll(".currency");
+const number = document.getElementById("text");
+const output = document.getElementById("output-amt");
 
-    
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exchangerate-api.p.rapidapi.com",
+    "X-RapidAPI-Key": "077f3a113fmsh70b5629f45bcfa2p114632jsnec97fbc6b6a9",
+  },
+};
+
+fetch("https://exchangerate-api.p.rapidapi.com/rapid/latest/USD", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
